3f7ffd1624bd5c7781cfe9ca5930ecad1c5fe956
AsadLiam <asadullah.itcgcs@gmail.com>
2018-09-26 13:08:49 +0500

    first commit but the project isn't complete yet

---
+++ social_site/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def dashboard(request):
+	context = {'section': 'dashboard'}
+	return render(request, 'account/dashboard.html', context)
